b0e9bd06c711184fe24adf34bb4e53b8266d9271
Charles Lee <charleslee90@gmail.com>
2016-04-29 12:49:40 +0800

    Add parallax effect

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+// $(function() {
+
+//   var $window = $(window);
+//   var $wrapper = $('#home-wrapper');
+//   $wrapper.css({'height': ($wrapper.height() - 458.857) + 'px'});
+
+//   $('section[data-type="background"]').each(function() {
+//     var $bgobj = $(this);
+
+//     $window.scroll(function() {
+//       var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+//       var coords = yPos + 'px';
+//       $('#home-wrapper .container').css({ transform: 'translateY(' + yPos*4 + 'px)' });
+//     }) // end window scroll
+//   })
+// })
