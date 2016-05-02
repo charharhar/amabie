03c082533bf428907d70d09f37bafa09d6bf0a4e
Charles Lee <charleslee90@gmail.com>
2016-05-02 21:23:45 +0800

    Complete home page, missing responsive design

--- public/js/app.js
+++ public/js/app.js
@@ -14,3 +14,12 @@
 //     }) // end window scroll
 //   })
 // })
+
+$(document).ready(function(){
+  $('.slider-carousel').slick({
+    prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+    dots: true,
+
+  });
+});
