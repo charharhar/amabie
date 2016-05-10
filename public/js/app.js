73f1e85c1b90331f8808744d6c1744657f88ec03
Charles Lee <charleslee90@gmail.com>
2016-05-10 18:33:39 +0800

    Complete more misc pages, WIP register

--- public/js/app.js
+++ public/js/app.js
@@ -17,12 +17,6 @@
 //   })
 // })
 
-// UTILITY FUNCTIONS
-// Changes all PRIVACY OPTIONS to the selected value from the dropdown menu
-function changeSelected(target, value) {
-  target.val(value.val());
-}
-
 $(function(){
   // VARIABLES DECLARED HERE
   var $selectAll = $('#select-all');
@@ -33,7 +27,52 @@ $(function(){
     changeSelected($selectBox, $selectAll);
   })
 
-  // ACTIVATING SLIDER
+  var findout_options = ["Word of mouth","Radio","TV","Print","Outdoor","Social Media","From a Friend","Advertisement"];
+
+  //populate find-out select box
+  for (i = 0; i < findout_options.length; i++) {
+    $('#find-out').append($('<option />').val(i).html(findout_options[i]));
+  }
+
+  var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+  //populate countries select box
+  for (i = 0; i < country_list.length; i++) {
+    $('#countries').append($('<option />').val(i).html(country_list[i]));
+  }
+
+  var monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  //populate years select box
+  for (i = new Date().getFullYear(); i > 1900; i--){
+    $('#years').append($('<option />').val(i).html(i));
+  }
+  //populate months select box
+  for (i = 1; i < 13; i++){
+    $('#months').append($('<option />').val(monthNames[i-1]).html(monthNames[i-1]));
+  }
+  //populate Days select box
+  updateNumberOfDays();
+
+  //Listening for change events
+  $('#years, #months').change(function(){
+    updateNumberOfDays();
+  });
+
+  // Activating home page slider "slick"
   $('.slider-carousel').slick({
     prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
     nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
@@ -41,3 +80,29 @@ $(function(){
   });
 
 });
+
+
+// UTILITY FUNCTIONS
+// ==========================
+
+// Changes all PRIVACY OPTIONS to the selected value from the dropdown menu
+function changeSelected(target, value) {
+  target.val(value.val());
+}
+
+//function to update the days based on the current values of month and year
+function updateNumberOfDays(){
+  $('#days').html('<option value="">Day</option>');
+  month = $('#months').val();
+  year = $('#years').val();
+  days = daysInMonth(month, year);
+
+  for(i=1; i < days+1 ; i++){
+    $('#days').append($('<option />').val(i).html(i));
+  }
+}
+
+// New date object with correct days in month
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
