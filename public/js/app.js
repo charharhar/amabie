ec9ed57833b13d4d18fc32846763a1a34b44533b
Charles Lee <charleslee90@gmail.com>
2016-05-24 18:54:58 +0800

    Fix bug round 2

--- public/js/app.js
+++ public/js/app.js
@@ -32,6 +32,8 @@ $(function(){
   var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
   var month_names = ['January','February','March','April','May','June','July','August','September','October','November','December', ];
 
+  var $dropdownToggle = $('.dropdown-toggle');
+
   // --------------------------------------------
   //              DOM MANIPULATION
   // --------------------------------------------
@@ -72,7 +74,7 @@ $(function(){
   })
 
   // Tab handler in my-community.js
-  $('a[data-tabName').on('click', function() {
+  $('a[data-tabName]').on('click', function() {
     var tabId = '#' + $(this).attr('data-tabName');
 
     $('.community').css('display', 'none');
@@ -144,16 +146,33 @@ $(function(){
 
   })
 
-  $('.dropdown-toggle').on('click', function(e) {
-    if ($(this).children().hasClass('fa-chevron-down')) {
-      $(this).children().removeClass('fa-chevron-down');
-      $(this).children().addClass('fa-chevron-up')
-    } else {
-      $(this).children().removeClass('fa-chevron-up');
-      $(this).children().addClass('fa-chevron-down')
-    }
+  $dropdownToggle.on('click', function(e) {
 
-    $(this).toggleClass('dropdownActive');
+    if ($(window).width() <= 768) {
+
+      if ($(this).children().hasClass('fa-chevron-down')) {
+        $(this).children().removeClass('fa-chevron-down');
+        $(this).children().addClass('fa-chevron-up')
+      } else {
+        $(this).children().removeClass('fa-chevron-up');
+        $(this).children().addClass('fa-chevron-down')
+      }
+
+      $(this).toggleClass('dropdownActive');
+
+    }
+  })
+
+  if ( $(window).width() > 768 ) {
+    $dropdownToggle.addClass('disabled');
+  }
+
+  $(window).on('resize', function() {
+    if ( $(window).width() > 768 && !$dropdownToggle.hasClass('disabled') ) {
+      $dropdownToggle.addClass('disabled');
+    } else if ( $(window).width() <= 768 && $dropdownToggle.hasClass('disabled') ) {
+      $dropdownToggle.removeClass('disabled');
+    }
   })
 
 
